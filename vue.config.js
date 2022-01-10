f1182621b32e5f2d17ad9b3e97b5bd7af683f9f1
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2022-01-09 20:04:32 -0600

    fix: wrong config file

--- vue.config.js
+++ vue.config.js
@@ -16,6 +16,9 @@ module.exports = {
     iconPaths: {
       favicon32: 'img/icons/favicon-32x32.png',
       favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: null,
+      maskIcon: null,
+      msTileImage: null,
     },
   },
 };
