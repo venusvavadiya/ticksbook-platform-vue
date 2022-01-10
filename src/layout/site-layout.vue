94374f8f086dd31afbf28a9c47f4be83be65bed1
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2022-01-09 20:53:51 -0600

    feat: dark mode (#4)
    * fix: wrong config file
    
    * feat: logo height
    
    * feat: dark theme preference

--- src/layout/site-layout.vue
+++ src/layout/site-layout.vue
@@ -2,9 +2,35 @@
   v-app
     v-app-bar(app elevation="0")
       v-row(justify="center")
-        router-link(:to="{ name: 'index' }")
-          v-img(src="../assets/logo.svg" width="48")
+        v-col(cols="auto")
+          router-link(:to="{ name: 'index' }")
+            v-img(src="../assets/logo.png" width="40")
 
     v-main
       slot
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+function isDarkModePreference(): boolean {
+  const query = '(prefers-color-scheme: dark)';
+  return window.matchMedia(query).matches;
+}
+
+function addDarkModePreferenceEventListener(listener: (x: boolean) => void): void {
+  const query = '(prefers-color-scheme: dark)';
+  window.matchMedia(query).addEventListener('change', (e) => {
+    listener(e.matches);
+  });
+}
+
+export default Vue.extend({
+  created(): void {
+    this.$vuetify.theme.dark = isDarkModePreference();
+    addDarkModePreferenceEventListener((isDark) => {
+      this.$vuetify.theme.dark = isDark;
+    });
+  },
+});
+</script>
