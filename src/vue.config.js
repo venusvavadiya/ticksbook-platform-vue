acdb6ce44b1319cbae52ddfa678e0373a83dc4f5
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2022-01-09 19:55:47 -0600

    feat: pwa

---
+++ src/vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  pwa: {
+    name: 'TicksBook',
+    display: 'standalone',
+    themeColor: '#272727',
+    msTileColor: '#272727',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    backgroundColor: '#000000',
+    startUrl: 'https://ticksbook-platform-vue.vercel.app',
+
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+    },
+  },
+};
