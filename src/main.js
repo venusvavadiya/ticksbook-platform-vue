ce7254e0e0b1c4523556ae641e1cde003f6782a3
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2022-01-11 09:15:43 -0600

    chore: main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import './registerServiceWorker';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 import auth0 from '@/plugins/auth0';
 import i18n from './plugins/i18n';
 import router from './plugins/router';
@@ -14,14 +12,12 @@ Vue.use(auth0, {
   clientId: process.env.VUE_APP_AUTH0_CLIENT_ID ?? 'vxEMhugPYXIJ19jrDAw6ICDbI5ZkQTdU',
   domain: process.env.VUE_APP_AUTH0_DOMAIN ?? 'sn-main-development.us.auth0.com',
 
-  onFailure(e: unknown) {
+  onFailure(e) {
     // eslint-disable-next-line no-console
     console.log(e);
   },
 
-  async onSuccess(appState: unknown) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
+  async onSuccess(appState) {
     await router.push(appState?.targetUrl ?? window.location.pathname);
   },
 });
